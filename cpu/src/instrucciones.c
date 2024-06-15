d96edae3dcbd9dd861d050157e451adf800eb5d4
Patricio Garcia Farrell <pgarciafarrell@frba.utn.edu.ar>
2024-06-15 20:47:39 +0000

    traduzco un poco mejor el stdin

--- cpu/src/instrucciones.c
+++ cpu/src/instrucciones.c
@@ -2,10 +2,8 @@
 
 //static char* _recibir_instruccion(int socket_cliente);
 static u_int8_t _get_identificador(char* identificador_txt);
-static char* _get_nombre_instruccion(u_int8_t instruccion);
 static u_int8_t _cantidad_parametros(u_int8_t identificador);
 static t_list* _parametros_instruccion(char* instruccion, u_int8_t cantidad_parametros);
-static void _mostrar_parametros(t_instruccion* instruccion, u_int8_t cantidad_parametros);
 //instrucciones
 static void _set(t_instruccion* instruccion);
 static void _sum(t_instruccion* instruccion);
@@ -107,70 +105,6 @@ static u_int8_t _get_identificador(char* identificador_txt){
 	return identificador;
 }
 
-static char* _get_nombre_instruccion(u_int8_t instruccion){
-	char* nombre = string_new();
-	switch(instruccion){
-		case SET:
-			string_append(&nombre, "SET");
-			break;
-		case MOV_IN:
-			string_append(&nombre, "MOV_IN");
-			break;
-		case MOV_OUT:
-			string_append(&nombre, "MOV_OUT");
-			break;
-		case SUM:
-			string_append(&nombre, "SUM");
-			break;
-		case SUB:
-			string_append(&nombre, "SUB");
-			break;
-		case JNZ:
-			string_append(&nombre, "JNZ");
-			break;
-		case RESIZE:
-			string_append(&nombre, "RESIZE");
-			break;
-		case COPY_STRING:
-			string_append(&nombre, "COPY_STRING");
-			break;
-		case WAIT:
-			string_append(&nombre, "WAIT");
-			break;
-		case SIGNAL:
-			string_append(&nombre, "SIGNAL");
-			break;
-		case IO_GEN_SLEEP:
-			string_append(&nombre, "IO_GEN_SLEEP");
-			break;
-		case IO_STDIN_READ:
-			string_append(&nombre, "IO_STDIN_READ");
-			break;
-		case IO_STDOUT_WRITE:
-			string_append(&nombre, "IO_STDOUT_WRITE");
-			break;
-		case IO_FS_CREATE:
-			string_append(&nombre, "IO_FS_CREATE");
-			break;
-		case IO_FS_DELETE:
-			string_append(&nombre, "IO_FS_DELETE");
-			break;
-		case IO_FS_TRUNCATE:
-			string_append(&nombre, "IO_FS_TRUNCATE");
-			break;
-		case IO_FS_WRITE:
-			string_append(&nombre, "IO_FS_WRITE");
-			break;
-		case IO_FS_READ:
-			string_append(&nombre, "IO_FS_READ");
-			break;
-		case EXIT:
-			string_append(&nombre, "EXIT");
-			break;
-	}
-	return nombre;
-}
-
 static u_int8_t _cantidad_parametros(u_int8_t identificador){
 	u_int8_t cantidad = 0;
 	switch(identificador){
@@ -255,53 +189,6 @@ static t_list* _parametros_instruccion(char* instruccion, u_int8_t cantidad_para
 	return parametros;
 }
 
-static void _mostrar_parametros(t_instruccion* instruccion, u_int8_t cantidad_parametros){
-	//log_protegido_cpu("_mostrar_parametros ");
-	char* nombre_instruccion = _get_nombre_instruccion(instruccion->identificador);
-	// if(cantidad_parametros == 0){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s>",pid,nombre_instruccion);
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);
-	// }else if(cantidad_parametros == 1){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s, %s>",pid,nombre_instruccion,list_get(instruccion->parametros,0));
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);		
-	// }else if(cantidad_parametros == 2){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s, %s, %s>",pid,nombre_instruccion,list_get(instruccion->parametros,0),list_get(instruccion->parametros,1));
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);
-	// }else if(cantidad_parametros == 3){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s, %s, %s, %s>",pid,nombre_instruccion,list_get(instruccion->parametros,0),list_get(instruccion->parametros,1),list_get(instruccion->parametros,2));
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);
-	// }else if(cantidad_parametros == 4){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s, %s, %s, %s, %s>",pid,nombre_instruccion,list_get(instruccion->parametros,0),list_get(instruccion->parametros,1),list_get(instruccion->parametros,2),list_get(instruccion->parametros,3));
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);
-	// }else if(cantidad_parametros == 5){
-	// 	char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s, %s, %s, %s, %s, %s>",pid,nombre_instruccion,list_get(instruccion->parametros,0),list_get(instruccion->parametros,1),list_get(instruccion->parametros,2),list_get(instruccion->parametros,3),list_get(instruccion->parametros,4));
-	// 	//log_protegido_cpu(mensaje_log);
-	// 	free(mensaje_log);
-	// }
-	// free(nombre_instruccion);
-	
-	// Prepara el mensaje con el nombre de la instrucción
-    char* mensaje_log = string_from_format("PID: <%d> - Ejecutando: <%s", contexto_cpu->pid, nombre_instruccion);
-    
-    // Agrega los parámetros al mensaje
-    for (int i = 0; i < cantidad_parametros; i++) {
-        char* parametro = list_get(instruccion->parametros, i);
-        mensaje_log = string_from_format("%s, %s", mensaje_log, parametro);
-    }
-    
-    // Cierra el mensaje
-    mensaje_log = string_from_format("%s>", mensaje_log);
-    
-    // Loggea el mensaje
-    //log_protegido_cpu(mensaje_log);
-    
-	log_warning(logger_cpu, "Necesito hacer un free aca?");
-}
 
 void devolver_contexto_a_dispatch(int motivo, t_instruccion* instruccion){
 	log_info(logger_cpu,"[DEVUELVO CONTEXTO]: -- PCB -- PID <%d> - PC<%d>",contexto_cpu->pid,contexto_cpu->program_counter);                
@@ -445,6 +332,22 @@ void ejecutar_proceso(){
 				motivo_desalojo = IO_GEN_SLEEP;
 				break;
 			case IO_STDIN_READ:
+				// IO_STDIN_READ (Interfaz, Registro Dirección, Registro Tamaño): 
+				// Esta instrucción solicita al Kernel que mediante la interfaz ingresada se 
+				// lea desde el STDIN (Teclado) un valor cuyo tamaño está delimitado por el valor 
+				// del Registro Tamaño y el mismo se guarde a partir de la Dirección Lógica almacenada 
+				// en el Registro Dirección.
+				char *registro_direccion = list_get(inst_decodificada->parametros, 0);
+    			char *registro_tamano = list_get(inst_decodificada->parametros, 1);
+				info_registro_cpu registro_dir_info = _get_direccion_registro(registro_direccion);
+				info_registro_cpu registro_tam_info = _get_direccion_registro(registro_tamano);
+
+				uint8_t valor_direccion_logica = *(uint8_t*)registro_dir_info.direccion;
+				uint8_t valor_tamano_a_escribir = *(uint8_t*)registro_tam_info.direccion;
+				//tomar la direccion logica y calcular la o las fisicas necesarias para escribir el tamano especificado.
+				//por cada direccion fisica hacer un "agregar_a_pedido_memoria" con esa direccion, el parametro "prueba1" puede tener
+				//cualquier valor aca porque se va a completar con datos de la interfaz despues.
+
 				log_warning(logger_cpu, "hay que traducir la direccion de memoria");
 				uint32_t dir_prueba = 99; //cuadno este la traduccion cambiar
 				uint32_t tam_prueba = 20; //esto seria el tamaño de pagina (max)
